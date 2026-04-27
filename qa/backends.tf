dcade0f4f67c21ad69ede5bb8e029a44a4efeee2
terraforn_user <terraforn_user@example.com>
2026-04-27 20:15:18 +0530

    qa changes are done

---
+++ qa/backends.tf
@@ -0,0 +1,21 @@
+/*
+terraform {
+  cloud {
+    organization = "ORGNAME"
+
+    workspaces {
+      name = "qa_tf_resources_ws"
+    }
+  }
+}
+*/
+
+terraform {
+  backend "s3" {
+    bucket = "qa-state-locking-bucket"
+    key    = "qa.tfstate"
+    region = "us-east-1"
+    #dynamodb_table = "terraform-lock"
+    use_lockfile = true
+  }
+}
